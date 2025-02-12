18b20fc6e700a9fabef5073c56060952642468c5
Sameem <nsam603@gmail.com>
2025-02-12 16:54:03 +0530

    mini project number gussing game

--- RandomNumber.py
+++ RandomNumber.py
@@ -17,4 +17,28 @@ random_number = r.randint(0,range_cap)
 print(random_number)
 
 
+guesses = 0
+while True:
+  guesses += 1
+  user_guess = int(input("enter the number"))
+  if user_guess ==random_number:
+   print('correct guess') 
+   break
 
+  elif user_guess > random_number:
+     print('you were above the number')
+     if guesses < 3:
+      continue
+     else:
+        print('wrong answer, guess again')
+        break
+  elif user_guess <  random_number:
+     print("you were below the number")
+     if guesses < 3:
+      continue
+     else:
+        print('wrong answer, guess again')
+        break
+ 
+  
+  
