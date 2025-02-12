fc757d754d702822a04df0fc2ef5abc4b6692df9
Sameem <nsam603@gmail.com>
2025-02-12 14:47:36 +0530

    first question

--- main.py
+++ main.py
@@ -9,11 +9,32 @@ print('lets play 😁')
 
 answer = input("what is the full form of cpu").lower()
 if answer == 'central processing unit':
- SCORE_COUNT =+ 1
+ SCORE_COUNT += 1
  print(f'{answer} is correct answer. score  = {SCORE_COUNT}')
 
 else:
- SCORE_COUNT =-1
+ SCORE_COUNT -=1
  print(f'{answer} is wrong answer, correct answer is"central processing unit"  score = {SCORE_COUNT}')
+
+
+answer = input("what does GPU stand for?").lower()
+if answer == 'graphics process unit':
+ SCORE_COUNT += 1
+ print(f'{answer} is correct answer. score  = {SCORE_COUNT}')
+
+else:
+ SCORE_COUNT -=1
+ print(f'{answer} is wrong answer, correct answer is"central processing unit"  score = {SCORE_COUNT}')
+
+answer = input("what does RAM stand for").lower()
+if answer == 'random access memory':
+ SCORE_COUNT += 1
+ print(f'{answer} is correct answer. score  = {SCORE_COUNT}')
+
+else:
+ SCORE_COUNT -=1
+ print(f'{answer} is wrong answer, correct answer is"random access memory"  score = {SCORE_COUNT}')
+
+print (f'{(SCORE_COUNT/3)*100}%')
 
  
