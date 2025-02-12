8e7175f9eecdf0d6d9d484dbbaae4a42168ccd65
Sameem <nsam603@gmail.com>
2025-02-12 15:11:29 +0530

    mini project number gussing game

--- RandomNumber.py
+++ RandomNumber.py
@@ -1,5 +1,20 @@
 import random as r
 
 
-random_number = r.randrange(1,10)
+range_cap = input("input the top range of the number")
+
+if  range_cap.isdigit():
+    range_cap = int(range_cap)
+    if range_cap < 0:
+        print('enter a number greater than 0')
+        quit()
+else:
+    print('Enter a number')
+    quit()
+
+
+random_number = r.randint(0,range_cap)
 print(random_number)
+
+
+
