aa0abbc8b5ec1adda6da829d36cdca3214e4f8b8
Sameem <nsam603@gmail.com>
2025-02-14 16:16:58 +0530

    this is the setup of pig game

--- pig.py
+++ pig.py
@@ -10,3 +10,20 @@ def roll():
 value = roll()
 print(value)
 
+
+while True:
+  players = input('Enter the number of players (2-4)')
+  if players.isdigit():
+     players = int(players)
+     if 2 <= players <= 4:
+      break
+     
+     else:
+      print('must be between 2 - 4 players:') 
+  else:
+    print('Invalid,try again')    
+
+print(players)
+
+#1:45:15
+
