894e2889cdf59cdf623dde19145d0b7a3f365881
Sameem <nsam603@gmail.com>
2025-02-14 15:53:49 +0530

    project completed, using cryptography fernet

--- passwordmanager.py
+++ passwordmanager.py
@@ -1,12 +1,33 @@
-master_pwd = input('what is the master password')
+from cryptography.fernet import Fernet as ff
 
 
 
+'''def write_key():
+  key = f.generate_key()
+  with open("key.key", "wb") as key_file:
+    key_file.write(key)
+
+write_key()
+'''
+
+def loadkey():
+  file =  open("key.key", "rb")
+  key = file.read()
+  file.close()
+  return key
+
+
+master_pwd = input('what is the master password') 
+key  = loadkey() + master_pwd.encode()
+fer = ff(key)
 
 def view():
    with open('passwords.txt','r') as f:
     for line in f.readlines():
-     print(line.rstrip())
+     data = line.rstrip()
+     user, passw = data.split("|")
+     print("User:", user , "Password:", fer.decrypt(passw.encode()).decode())
+
 
 
  
@@ -15,7 +36,7 @@ def add():
   pwd = input( 'password:')  
   
   with open('passwords.txt','a') as f:
-    f.write(name + '|' + pwd + "\n")
+    f.write(name + '|' + fer.encrypt(pwd.encode()).decode() + "\n")
 while True:
   mode = input('would like to add a new password or view existing ones (view)').lower()
   if mode == 'x':
