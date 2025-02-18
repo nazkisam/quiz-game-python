9312cee5695899cb75503745005f8ced7627127a
Sameem <nsam603@gmail.com>
2025-02-18 16:17:18 +0530

    turtle race setup

---
+++ turtleRace.py
@@ -0,0 +1,37 @@
+
+def get_num_of_racers():
+  racers = 0
+  while True:
+    racers = input("enter the number of turtles from (2-10): ")
+    
+    if racers.isdigit():
+     racers = int(racers)
+     
+     
+    else:
+     print('wrong input, should be digit')
+     continue
+
+    if 2<= racers <=10: 
+      return racers
+
+    else:
+      print('wrong input')
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(get_num_of_racers())
+
+
